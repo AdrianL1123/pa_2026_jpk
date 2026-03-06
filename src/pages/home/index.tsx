9da1dbe0f6ac62e536dd52871663c6c941f00a7a
[AdrianL] <[adrianlee061123@gmail.com]>
2026-03-06 14:57:28 +0800

    add icon

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,10 @@
+import { Notebook } from "lucide-react";
+
 export default function Home() {
   return (
     <div className="min-h-screen flex items-center py-24 gap-3 flex-col">
-      <p className="text-3xl font-bold text-green-800">
-        Welcome to the Warm-up Exercise Planner
+      <p className="text-3xl font-bold text-green-800 flex items-center gap-2">
+        Welcome to the Warm-up Exercise Planner <Notebook className="size-8" />
       </p>
       <p className="text-slate-500">
         This is the homepage. Use the navigation links to explore!
